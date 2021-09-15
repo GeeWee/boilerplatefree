03ae6fd18f7655d1624eb12b71c57bd3e7b7490b
geewee <gustavwengel@gmail.com>
2021-09-15 10:07:25 +0200

    pr cleanup

--- Source/BoilerplateFree/Logging/AddNLogAttribute.cs
+++ Source/BoilerplateFree/Logging/AddNLogAttribute.cs
@@ -3,7 +3,7 @@ namespace BoilerplateFree
     using System;
 
     /// <summary>
-    /// Use this to add an NLogger on your class under the _logger scope.
+    /// Use this to add a static NLogger logger on your class with the _logger name.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public class AddNLogAttribute: Attribute
--- Source/BoilerplateFree/Logging/AddSerilogAttribute.cs
+++ Source/BoilerplateFree/Logging/AddSerilogAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace BoilerplateFree
 {
     /// <summary>
-    /// Use this to add a Serilog logger on your class under the _logger scope. 
+    /// Use this to add a static Serilog logger on your class with the _logger name. 
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public class AddSerilogAttribute: Attribute
